697a7822b01c33a0d40e996ac44e6fcf72c96a13
ebruno-panda <ebruno@DESKTOP-5KT47FQ.localdomain>
2020-10-13 13:17:07 -0400

    Takes in user input

--- proj1.cpp
+++ proj1.cpp
@@ -4,3 +4,11 @@
 using namespace std;
 
 int main() {
+	string contestar; //(5) | stores user input
+        getline(cin, contestar); //(1)
+
+        int N = 26 + (2935002 % 11); //(2) ID has a 0 in front | N = 26 + 4 |base N = 30
+  
+        /*Outputs 0 if input length > 10*/
+        if (contestar.length() > 10){cout << 0 << endl;} //(3)
+        else { //(4)
